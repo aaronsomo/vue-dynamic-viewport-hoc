9368b1fc24409c33b2973546ca1ef0174fc02bb1
Aaron Southammavong <53191613+aaronsomo@users.noreply.github.com>
2021-06-08 12:43:31 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app>
+    <v-main>
+      <ResizablePage>
+        <template v-slot:header>
+          <h1 class="text-center">Header</h1>
+        </template>
+        <template v-slot:toolbar>
+          <h3 class="text-center">Toolbar</h3>
+        </template>
+        <template v-slot:filter>
+          <p>Filter</p>
+        </template>
+        <!-- <template v-slot:table="tableProps">
+          <v-data-table
+            :height="tableProps.tableHeight"
+            :headers="tableHeaders"
+            :items="tableItems"
+            :items-per-page="20"
+            :footer-props="{ itemsPerPageOptions: [20, 30, 50] }"
+          >
+          </v-data-table>
+        </template> -->
+        <template v-slot:table="tableProps">
+          <DataTable v-bind:tableProps="tableProps" />
+        </template>
+      </ResizablePage>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ResizablePage from "./components/resizable-page";
+import DataTable from "./components/data-table";
+
+export default {
+  name: "App",
+  components: {
+    ResizablePage,
+    DataTable,
+  },
+  computed: {
+    parentProps() {
+      return {};
+    },
+  },
+  // data() {
+  //   return {
+  //     tableHeaders: [
+  //       { text: "Name", value: "name" },
+  //       { text: "Value", value: "val" },
+  //     ],
+  //     tableItems: [],
+  //   };
+  // },
+  // mounted() {
+  //   for (let i = 0; i < 50; i++) {
+  //     let obj = {};
+  //     obj.name = "Item " + (i + 1);
+  //     obj.val = (i + 1) * 10;
+  //     this.tableItems.push(obj);
+  //   }
+  // },
+};
+</script>
+
---
+++ src/components/data-table.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-data-table
+    :height="tableProps.tableHeight"
+    :headers="tableHeaders"
+    :items="tableItems"
+    :items-per-page="20"
+    :footer-props="{ itemsPerPageOptions: [20, 30, 50] }"
+  >
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: "DataTable",
+  // props: ["tableProps"],
+  props: {
+    tableProps: { type: Object },
+  },
+  data() {
+    return {
+      tableHeaders: [
+        { text: "Name", value: "name" },
+        { text: "Value", value: "val" },
+      ],
+      tableItems: [],
+      // tableHeight: 300,
+    };
+  },
+  mounted() {
+    for (let i = 0; i < 50; i++) {
+      let obj = {};
+      obj.name = "Item " + (i + 1);
+      obj.val = (i + 1) * 10;
+      this.tableItems.push(obj);
+    }
+  },
+  // computed: {
+  //   calcHeight() {
+  //     return {
+  //       height: this.tableHeight
+  //     }
+  //   }
+  // }
+};
+</script>
